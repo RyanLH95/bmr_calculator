37cb8cb5b6eba249bd0f95fac2f2b1fc4ca6aef7
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-10-06 14:34:42 +0100

    Add elements to App.jsx file to create page for bmr calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+      <h1 className="title">BMR Calculator</h1>
+        <form className="form">
+          <button type="button" className="btn btn-dark us">US Units</button>
+          <button type="button" className="btn btn-dark metric">Metric Units</button>
+            <div className="bmr-box">
+              <h5>Gender</h5>
+              <label className="form-check-label" htmlFor="flexRadioDefault1">
+                Male
+                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+              </label>
+              <label className="form-check-label" htmlFor="flexRadioDefault2">
+                Female
+                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" defaultChecked/>
+              </label>
+              <h5 className="age">Age</h5>
+              <label className="age-box">
+                <input type="text" className="box"/>
+              </label>
+              <div className="metric-unit">
+                <h5 className="height">Height</h5>
+                <label class="metric-height">
+                  <input type="text" className="box"/>
+                  cm
+                </label>
+                <h5 className="weight">Weight</h5>
+                <label>
+                  <input type="text" className="box"/>
+                  kg
+                </label>
+              </div>
+              <div className="us-unit">
+                <h5 className="height">Height</h5>
+                <label className="us-height">
+                  <input type="text" className="box"/>
+                  feet
+                  <input type="text" className="box"/>
+                  inches
+                </label>
+                <h5 className="weight">Weight</h5>
+                <label>
+                  <input type="text" className="box"/>
+                  lbs
+                </label>
+              </div>
+              <button type="button" className="btn btn-dark calc">Calculate BMR</button>
+              <button type="button" className="btn btn-dark clear">Clear</button>
+            </div>
+        </form>
+      </>
   )
 }
-
-export default App
